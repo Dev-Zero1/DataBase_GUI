a2cdb905cdf0332f5e731716d353bf889b9ea938
Dev_Zero1 <36249579+Dev_Zero1@users.noreply.github.com>
2018-06-05 10:24:49 -0700

    update on db

--- createSQLgui.java
+++ createSQLgui.java
@@ -1,5 +1,5 @@
-//PHP/SQL GUI to easily create queries, statements, 
-//and web pages with a table or other db tables imported potentially. 
+//PHP/SQL GUI to easily create queries, statements,
+//and web pages with a table or other db tables imported potentially.
 
 import java.io.*;
 import java.util.*;
@@ -12,185 +12,183 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.geometry.Pos;
 import javafx.geometry.Insets;
- 
+
 import java.lang.IllegalArgumentException;
  /*
 	Interact with a database online
-	   put together 
-	   
+	   put together
+
 	    //design your own tables! - manage pt/clients/etc.
 		CREATE - add a table of people.7
 		INSERT
 		DROP
 		UPDATE
 		SELECT / display
-		
+
 		//hook up to db
 		//find available db's -- create obj arrays out of them.
-		
+
 		-add, remove, modify, etc
-		upload csv to become a table... rename? connect to db? create new db? 
- 
+		upload csv to become a table... rename? connect to db? create new db?
+
  */
-public class createSQLgui{   
+public class createSQLgui{
 /*
 assuming from a csv/data table containing columns/rows.
 */
-	LinkedList  <TextField> fieldList; 	 
-	LinkedList  <LinkedList<TextField>> fieldData; 	
-	
+	LinkedList  <TextField> fieldList;
+	LinkedList  <LinkedList<TextField>> fieldData;
+
 	private 	int		COL; 	//determined by number of columns across
 	private 	int 	ROW; 	//determined by number of rows down
-	
-		
+
+
 	//Button 	clear;
 	//Button 	submit;
     LinkedList <Button> del;
-	
+
 	LinkedList <HBox>  	recordBoxes; 	//rows of records - up to 300
 	HBox  			   	fieldBoxes;		// columns of metadata, up to 40
 	public VBox			container;		//the Vertical Box to house all headers/data.
 	public AnchorPane 	layout;			//final layout pane for the stage.
 	public HBox 		delBox;
-	 
-	
+
+
 	//data to potentially work with a database connection
-	private 	String 	db; 		
-	private 	String 	table;	
-	private 	String 	user; 	
+	private 	String 	db;
+	private 	String 	table;
+	private 	String 	user;
 	private		String 	pw;
 //--------------------------------------------------------------------------//
-		//function: createSQLgui() - default constructor for new calcGUI() objects. 
-//--------------------------------------------------------------------------//	
+		//function: createSQLgui() - default constructor for new calcGUI() objects.
+//--------------------------------------------------------------------------//
 	//default constructor no parameters.
-	protected createSQLgui() 
+	protected createSQLgui()
 	{
 		_default(); //initialize all elements to not-null states.
 		initDBvars();
- 
-		//setListeners();	
+
+		//setListeners();
 	}
 //--------------------------------------------------------------------------//
-		//function: _default() - no args, no return. 
+		//function: _default() - no args, no return.
 		//Initializes each element with an object/value. Fresh state/constructor.
-//--------------------------------------------------------------------------//	
+//--------------------------------------------------------------------------//
 	private void initDBvars(){
-		db 		="";
+		db 		="k";
 		table	="";
 		user	="";
 		pw		="";
-		
+
 	}
     private void setListeners(){
 		//clear.setOnAction (e ->{ Answer.setText("");  });
-		 
-	}	 
+
+	}
 	private void resetVals(){
-	 
+
 	}
 	private void setTextListeners(final int x){
 		//num[x].setOnAction(e -> {  });
 	}
 //--------------------------------------------------------------------------//
-		//function: _default() - no args, no return. 
+		//function: _default() - no args, no return.
 		//Initializes each element with an object/value. Fresh state/constructor.
-//--------------------------------------------------------------------------//	
+//--------------------------------------------------------------------------//
 
 
 
 
- //---------------------------------------------------------------------------//     
-                                 
-//---------------------------------------------------------------------------// 
+ //---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
 	private void _default(){
-		
+
 	COL =6;
 	ROW =3; //start with 3 rows. 2 for records, 1 for table headers.
 	delBox 			= new HBox ();
-	fieldData 		= new <LinkedList <TextField>> LinkedList(); 
-	
+	fieldData 		= new <LinkedList <TextField>> LinkedList();
+
 	//create the field HBox across ---->
-	fieldBoxes		= new HBox(5); 
-	
-	container 		= new VBox(5);	
+	fieldBoxes		= new HBox(5);
+
+	container 		= new VBox(5);
 	layout 			= new AnchorPane();
-	
+
 	//create the linked list of HBoxes for records, with the first one...
 	recordBoxes = new <HBox> LinkedList(Arrays.asList(new HBox(5),new HBox(5),new HBox(5)));
- 
+
 	//create the 6 starting fieldList text boxes (empty).
 	fieldList  	= new <TextField> 	LinkedList(Arrays.asList(new TextField("Labels"),new TextField(""),new TextField(""),new TextField(""),new TextField(""),new TextField("") ));
 	del 		= new <Button> 		LinkedList(Arrays.asList(new Button("X")  ,new Button("X")  ,new Button("X")  ,new Button("X")  ,new Button("X")  ,new Button("X")   ));
-	
+
 	//create an empty linked list of linkedlists for TextFields on fieldData going down columns parallel w/ fieldList.
-  
+
 	init_Fdata();
- 
+
 	//add the first six rows of linked list data for the field (one textfield for each columnn's field).
 	for(int y=0; y<COL; y++){
 		delBox.getChildren().add(del.get(y));
-		fieldBoxes.getChildren().add(fieldList.get(y));	 
+		fieldBoxes.getChildren().add(fieldList.get(y));
 	}
-	 
+
 	container.getChildren().addAll(delBox, fieldBoxes,recordBoxes.get(0),recordBoxes.get(1),recordBoxes.get(2));
 	layout.getChildren().add(container);
 	//set a titles
 	//ability to delete by row or column and shift up
-	}	
-//---------------------------------------------------------------------------//     
-                                 
-//---------------------------------------------------------------------------// 	
+	}
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
 	public void init_Fdata(){
-	
+
 		for(int c=0; c < COL; c++){
 			fieldData.add(new <TextField> LinkedList());
-		for(int r=0; r < ROW; r++){			
-		 
+		for(int r=0; r < ROW; r++){
+
 			//grab column's box, add the fieldData column's row to it.
 			fieldData.get(c).add(new TextField());
 			recordBoxes.get(r).getChildren().add(fieldData.get(c).getLast());
-		}		
+		}
 		}
 	}
-	
-	
-//---------------------------------------------------------------------------//     
-                                 
-//---------------------------------------------------------------------------//  	
+
+
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
 	public void addCol(){
-		
+
 		fieldData.add(new LinkedList <TextField>(Arrays.asList(new TextField(""))));
 		//..then fill it downwards!
-		COL++;		
+		COL++;
 	}
-//---------------------------------------------------------------------------//     
-                                 
-//---------------------------------------------------------------------------// 
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
 	public void addRow(){
-		
+
 		recordBoxes.add(new HBox(5));
 
-		for(int x=0; x<COL; x++){	 
+		for(int x=0; x<COL; x++){
 			fieldData.get(x).add(new TextField("x"));
-			//get each HBox row, add the associated fieldData column, very last fieldData boxes.	
-		}	
-		
-		for(int n=0; n<ROW; n++){	
-		for(int y=0; y<COL; y++){	
+			//get each HBox row, add the associated fieldData column, very last fieldData boxes.
+		}
+
+		for(int n=0; n<ROW; n++){
+		for(int y=0; y<COL; y++){
 			recordBoxes.get(n).getChildren().add(fieldData.get(y).getLast());
 		}
 		}
-		ROW++;	
+		ROW++;
 	}
-//---------------------------------------------------------------------------//     
-                                 
-//---------------------------------------------------------------------------// 
+//---------------------------------------------------------------------------//
+
+//---------------------------------------------------------------------------//
 	//add row empty to list - loop that returns HBox to add.
 	//remove row
 	//add column
 	//remove column
- 
+
 
 }//end class: createSQLgui
-
-
